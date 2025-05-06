ac4031da72106c88ded877159fcc56314450b026
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2025-05-06 15:52:22 +0200

    Manage profile section

--- js/components/themeToggle.mjs
+++ js/components/themeToggle.mjs
@@ -1,4 +1,26 @@
 export function themeToggle() {
+  const themeToggle = document.getElementById("themeToggle");
+  const isDarkMode = localStorage.getItem("theme") === "dark";
+
+  if (isDarkMode) {
+    document.documentElement.classList.add("dark");
+    themeToggle.checked = true;
+  }
+
+  themeToggle.addEventListener("change", () => {
+    if (themeToggle.checked) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  });
+}
+
+/*
+
+export function themeToggle() {
   const themeToggle = document.querySelector("#themeToggle");
   const savedTheme = localStorage.getItem("theme");
 
@@ -22,3 +44,4 @@ export function themeToggle() {
     });
   }
 }
+*/
