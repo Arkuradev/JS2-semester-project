0a8bcb2efa16f9f50133ea7f3adf587662e9ddfc
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2025-05-06 16:00:44 +0200

    Reverted themeToggle changes

--- js/components/themeToggle.mjs
+++ js/components/themeToggle.mjs
@@ -1,26 +1,4 @@
 export function themeToggle() {
-  const themeToggle = document.getElementById("themeToggle");
-  const isDarkMode = localStorage.getItem("theme") === "dark";
-
-  if (isDarkMode) {
-    document.documentElement.classList.add("dark");
-    themeToggle.checked = true;
-  }
-
-  themeToggle.addEventListener("change", () => {
-    if (themeToggle.checked) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  });
-}
-
-/*
-
-export function themeToggle() {
   const themeToggle = document.querySelector("#themeToggle");
   const savedTheme = localStorage.getItem("theme");
 
@@ -44,4 +22,3 @@ export function themeToggle() {
     });
   }
 }
-*/
